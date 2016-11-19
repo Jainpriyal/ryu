1dbe786c3e034d7ae1e9c55ce6893438c976d300
jainpriyal <jainpriyal168@gmail.com>
2016-11-19 02:12:08 -0500

    adding code to calculate link cost

--- ryu/app/qos_routing/qos_forwarding.py
+++ ryu/app/qos_routing/qos_forwarding.py
@@ -13,10 +13,10 @@ from ryu.lib.packet import packet
 from ryu.lib.packet import ethernet
 from ryu.lib.packet import ipv4
 from ryu.lib.packet import arp
-
+from ryu.lib import hub
 from ryu.topology import event, switches
 from ryu.topology.api import get_switch, get_link
-
+import constants
 import discover_topology
 import latency_detector
 
@@ -37,8 +37,20 @@ class QosForwarding(app_manager.RyuApp):
         self.topology = kwargs["topology_discovery"]
         self.latency_calculator = kwargs["latency_calculator"]
         self.datapaths = {}
-
-
+        self.latency_weight = 10 #weight assigned to latency
+        self.measure_thread = hub.spawn(self._metric_calculator)        
+         
+    def _metric_calculator(self):
+	i=0
+	while True:
+	     self._calculate_link_cost()
+	     if i ==5:
+	        self.show_link_cost()
+		i=0
+             else:
+		i=i+1
+             hub.sleep(1)
+	
     @set_ev_cls(ofp_event.EventOFPStateChange,
                 [MAIN_DISPATCHER, DEAD_DISPATCHER])
     def _state_change_handler(self, ev):
@@ -54,4 +66,34 @@ class QosForwarding(app_manager.RyuApp):
             if datapath.id in self.datapaths:
                 self.logger.debug('unregister datapath: %016x', datapath.id)
                 del self.datapaths[datapath.id]
-   
+
+    def _calculate_link_cost(self):
+	"""
+	    Function to calculate cost of links
+	"""
+        try:
+            for src in self.topology.database:
+                for dst in self.topology.database[src]:
+		    if src == dst:
+                        self.topology.database[src][dst]['cost'] = 0
+                        continue
+                    latency = self.topology.database[src][dst]['latency']
+		    cost = self.latency_weight*latency
+                    self.topology.database[src][dst]['cost'] = cost
+        except Exception as ex:
+            if self.topology is None:
+                self.topology = lookup_service_brick('topologydiscovery')
+            return
+
+    def show_link_cost(self):
+	"""
+	    Show link cost
+        """
+        if constants.SHOWCOST and self.topology is not None and self.topology.database.number_of_nodes()>0:
+            self.logger.info("\nsrc-dpid    dst-dpid               Cost")
+            self.logger.info("-----------------------------------------------")
+            for src in self.topology.database:
+                for dst in self.topology.database[src]:
+                    cost = self.topology.database[src][dst]['cost']
+                    self.logger.info("%s<-->%s : %s" % (src, dst, cost))
+
